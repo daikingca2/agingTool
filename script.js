0f4f8a6344af0887ef20ec1f8f0708b651f0ef05
xxxx xxxx <xxxxx@xxxxx.xx.xxx>
2015-06-08 17:41:54 +0900

    参考を削除 関数名を変更

--- script.js
+++ script.js
@@ -1,29 +1,14 @@
 //js
 
-//参考
-// <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
-// <script type="text/javascript">
-// $(function(){
-//     var count = setInterval(changeImg, 500);//ミリ秒数
-//     i = 1;
-//     function changeImg(){
-//         $("#idImgchng").children("img").attr("src","i" + i + ".png");
-//         if(i <= 3){	//4枚の画像を切替
-//             i++;
-//         }else{
-//             i = 1;
-//         }
-//     }
-// });
-// </script>
+
 
 $(function(){
-	var count = setInterval(changeImg,1000);
+	var count = setInterval(changeColor,1000);
 	//カウンター
 	color_i = 0;
 	//使いたい色をここに突っ込む
 	var color = ["#000000","#87CEEB","#F0E68C","#FF0000"];
-	function changeImg(){
+	function changeColor(){
 		$("#body").css("background-color",color[color_i]);
 		if(color_i <= color.length){ //配列clolorのlength
 			color_i++;

90fbaf4354cc6ede97c0764eecbc7efb5a523709
xxxx xxxx <xxxxx@xxxxx.xx.xxx>
2015-06-08 19:54:52 +0900

    現在時刻の表示を追加

--- script.js
+++ script.js
@@ -3,11 +3,13 @@
 
 
 $(function(){
-	var count = setInterval(changeColor,1000);
+
+	//背景色変更
+	var count = setInterval(changeColor,2000);
 	//カウンター
 	color_i = 0;
 	//使いたい色をここに突っ込む
-	var color = ["#000000","#87CEEB","#F0E68C","#FF0000"];
+	var color = ["#FF370A","#F90941","#F9F009","#009AF6","00F6B1"];
 	function changeColor(){
 		$("#body").css("background-color",color[color_i]);
 		if(color_i <= color.length){ //配列clolorのlength
@@ -17,4 +19,24 @@ $(function(){
 		}
 
 	}
+
+	//流れる物体
+	function boxSlide(){
+		$("#box").animate({left:'120%'},6000);
+		$("#box").animate({left:'-120%'},6000);
+	}
+
+	setInterval(boxSlide,2000);
+
+	setInterval( function(){
+    	$("#time").text(Date());
+  	}, 1000);
+
+
+
+
+
+
+
+
 })

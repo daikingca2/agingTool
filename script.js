e7f261dc4c66dcdc1cdb803dab52b07cbc6a1617
daikingca2 <daiking.ca2@gmail.com>
2016-04-28 23:50:23 +0900

    気になるところを修正

--- script.js
+++ script.js
@@ -1,44 +1,37 @@
-//js
+// js
 
-
-
-$(function(){
+$(function() {
 
 	//背景色変更
 	var count = setInterval(changeColor,2000);
+
 	//カウンター
 	color_i = 0;
+
 	//使いたい色をここに突っ込む
 	var color = ["#FF370A","#F90941","#F9F009","#009AF6","00F6B1"];
-	function changeColor(){
+	function changeColor() {
 		$("#body").css("background-color",color[color_i]);
-		if(color_i <= color.length){ //配列clolorのlength
+		if( color_i <= color.length ){ //配列clolorのlength
 			color_i++;
-		}else{
+		} else {
 			color_i = 0;
 		}
 
 	}
 
 	//流れる物体
-	function boxSlide(){
-		$("#box").animate({left:'120%'},6000);
-		$("#box").animate({left:'-120%'},6000);
+	function boxSlide() {
+		$("#box").animate( {left:'120%'}, 6000 );
+		$("#box").animate( {left:'-120%'}, 6000 );
 	}
 
 	setInterval(boxSlide,2000);
 
-	setInterval( function(){
+	setInterval( function() {
 		var d = new Date();
 		var str =d.toTimeString();
     	$("#time").text(str);
   	}, 1000);
-
-
-
-
-
-
-
 
 })
